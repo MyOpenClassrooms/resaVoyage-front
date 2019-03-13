ec648d61e67fe5b17a6bebe054fed13dfba45fe7
dikha <khadisankhe@gmail.com>
2019-03-13 12:15:53 +0100

    updating constant

---
+++ resaVoyage/src/app/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) {
+  }
+
+
+}
--- resaVoyage/src/shared/constant.ts
+++ resaVoyage/src/shared/constant.ts
@@ -1,4 +1,5 @@
 // API Services
 
-export const apiPrefix = 'http://localhost:9094';
+export const apiPrefix = 'http://localhost:9004';
 export const apiCommande = apiPrefix + '/commande';
+export const apiLogin = apiPrefix + '/auth';
