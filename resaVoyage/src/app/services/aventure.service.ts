94ca0b1f5747c36789f34c511c7e2164d9fd149c
benhadji <hadjiben@gmail.com>
2019-03-20 15:14:03 +0100

    homepage with aventures are displayed correctly

--- resaVoyage/src/app/services/aventure.service.ts
+++ resaVoyage/src/app/services/aventure.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {apiAventure, apiAventureById} from 'src/shared/constant';
+import {apiAventure, apiAventureById, apiCommande} from 'src/shared/constant';
+import {Observable} from "rxjs";
+import {Commande} from "../../shared/models/commande";
+import {Aventure} from "../../shared/models/aventure";
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +12,13 @@ export class AventureService {
 
   constructor(private http: HttpClient) {}
 
-  getAllAventures() {
+  getAllAventures(): Observable<Aventure[]> {
     /*const headers = new HttpHeaders({Authorization: 'Basic ' + btoa('test:test123')});*/
-    return this.http.get( apiAventure + '/aventure/getAll',
-      {
-        headers: new HttpHeaders()
-        /*.set('Access-Control-Allow-Origin', '*')*/
-          .set('Content-Type', 'application/x-www-form-urlencoded')
-        /* .set('Access-Control-Allow-Origin', '*')*/
-        //.set('Authorization', 'Basic ' + btoa('test:test123'))
-      })
-
+    return this.http.get<Aventure[]>( apiAventure + '/aventure/getAll');
   }
 
-  getAventureById(id:number){
-    return this.http.get(apiAventureById + '/aventure/' + id);
+  getAventureById(id:number):Observable<Aventure>{
+    return this.http.get<Aventure>(apiAventureById + '/aventure/' + id);
   }
 
 }
